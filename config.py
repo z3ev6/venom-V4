d661e15c7123b3c4833f44e69855d0d4e6474d40
z3ev6 <cursedlovedhell@gmail.com>
2024-07-29 20:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7374733018:AAHu5LZrQcfbpGidkD9FzJnVYbziG9JBDX0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Lakshay_03z")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6984603270")
 
 
 
